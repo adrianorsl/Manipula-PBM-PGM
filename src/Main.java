63c42c6cedc12469e8fa5a8b32389528a60bb9de
Adriano Zimmermann <adrianorslsc@hotmail.com>
2023-09-04 10:48:26 -0300

    atividade 6

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Main {
 	
+//Atividade 5-2 a e b	
 public static String Alterar_PGM_PBM(String inputFile2, int valorMaximo, String tipoImagem, int limite) throws IOException {
     	
     	// Carrega a imagem PGM de entrada
@@ -24,11 +25,13 @@ public static String Alterar_PGM_PBM(String inputFile2, int valorMaximo, String 
         FileWriter writer = new FileWriter(outputFile2);
         writer.write(tipoImagem + "\n");
         writer.write(largura + " " + altura + "\n");
+        //escreve o valor maximo dos bits
         writer.write(valorMaximo + "\n");
         
         for (int y = 0; y < altura; y++) {
             for (int x = 0; x < largura; x++) {
                 int bits = scanner.nextInt();
+                //faz checagens com o limite difinido
                 if (bits <= limite) {
                 	newBit = 0;
                 }else {
@@ -45,6 +48,46 @@ public static String Alterar_PGM_PBM(String inputFile2, int valorMaximo, String 
         return teste;
     }
 
+// Atividade 6 
+public static String Alterar_PGM_PBM_valor(String inputFile2, int valorMaximo, String tipoImagem, int limite, int valor1, int valor2) throws IOException {
+	
+	// Carrega a imagem PGM de entrada
+    FileInputStream inputStream2 = new FileInputStream(inputFile2);
+    Scanner scanner = new Scanner(inputStream2);
+    
+    // Lê o cabeçalho da imagem PGM
+    String tipoImagem2 = scanner.nextLine();
+    int largura = scanner.nextInt();
+    int altura = scanner.nextInt();
+    int valorMaximo2 = scanner.nextInt();
+    int newBit = 0;
+    	
+    String outputFile2 = "/home/adriano/Imagens/PDI/Manipula2.pgm"; // Caminho da imagem redimensionada de saída
+    FileWriter writer = new FileWriter(outputFile2);
+    writer.write(tipoImagem + "\n");
+    writer.write(largura + " " + altura + "\n");
+    writer.write(valorMaximo + "\n");
+    
+    for (int y = 0; y < altura; y++) {
+        for (int x = 0; x < largura; x++) {
+            int bits = scanner.nextInt();
+            if (bits <= limite) {
+            	newBit = valor1;
+            }else {
+            	newBit = valor2;
+            }
+            writer.write(newBit + " ");
+        }
+        writer.write("\n");
+    }
+    writer.close();
+	
+	String teste = "OK";
+    
+    return teste;
+}
+
+//Atividade 5-2 c inverte os valores dos bits
 public static String Negativo(String inputFile2, String tipoImagem) throws IOException {
 	
 	// Carrega a imagem PGM de entrada
@@ -60,6 +103,7 @@ public static String Negativo(String inputFile2, String tipoImagem) throws IOExc
     	
     String outputFile2 = "/home/adriano/Imagens/PDI/Negativo1.pbm"; // Caminho da imagem redimensionada de saída
     FileWriter writer = new FileWriter(outputFile2);
+    // Define o tipo da imagem
     writer.write(tipoImagem + "\n");
     writer.write(largura + " " + altura + "\n");
     writer.write(valorMaximo + "\n");
@@ -91,7 +135,7 @@ public static String Negativo(String inputFile2, String tipoImagem) throws IOExc
 		
 		Alterar_PGM_PBM(inputFile, 1, "P1", 128);
 		Negativo(inputFile2, "P1");
-		
+		Alterar_PGM_PBM_valor(inputFile, 255, "P2", 128, 0, 255);
 	}
 
 }
