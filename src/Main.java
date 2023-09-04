377110e907f1dfe502abf96d911d03b6a45e8ef0
Adriano Zimmermann <adrianorslsc@hotmail.com>
2023-09-04 10:21:28 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,97 @@
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+public class Main {
+	
+public static String Alterar_PGM_PBM(String inputFile2, int valorMaximo, String tipoImagem, int limite) throws IOException {
+    	
+    	// Carrega a imagem PGM de entrada
+        FileInputStream inputStream2 = new FileInputStream(inputFile2);
+        Scanner scanner = new Scanner(inputStream2);
+        
+        // Lê o cabeçalho da imagem PGM
+        String tipoImagem2 = scanner.nextLine();
+        int largura = scanner.nextInt();
+        int altura = scanner.nextInt();
+        int valorMaximo2 = scanner.nextInt();
+        int newBit = 0;
+        	
+        String outputFile2 = "/home/adriano/Imagens/PDI/Manipula1.pbm"; // Caminho da imagem redimensionada de saída
+        FileWriter writer = new FileWriter(outputFile2);
+        writer.write(tipoImagem + "\n");
+        writer.write(largura + " " + altura + "\n");
+        writer.write(valorMaximo + "\n");
+        
+        for (int y = 0; y < altura; y++) {
+            for (int x = 0; x < largura; x++) {
+                int bits = scanner.nextInt();
+                if (bits <= limite) {
+                	newBit = 0;
+                }else {
+                	newBit = 1;
+                }
+                writer.write(newBit + " ");
+            }
+            writer.write("\n");
+        }
+        writer.close();
+    	
+    	String teste = "OK";
+        
+        return teste;
+    }
+
+public static String Negativo(String inputFile2, String tipoImagem) throws IOException {
+	
+	// Carrega a imagem PGM de entrada
+    FileInputStream inputStream2 = new FileInputStream(inputFile2);
+    Scanner scanner = new Scanner(inputStream2);
+    
+    // Lê o cabeçalho da imagem PGM
+    String tipoImagem2 = scanner.nextLine();
+    int largura = scanner.nextInt();
+    int altura = scanner.nextInt();
+    int valorMaximo = scanner.nextInt();
+    int newBit = 0;
+    	
+    String outputFile2 = "/home/adriano/Imagens/PDI/Negativo1.pbm"; // Caminho da imagem redimensionada de saída
+    FileWriter writer = new FileWriter(outputFile2);
+    writer.write(tipoImagem + "\n");
+    writer.write(largura + " " + altura + "\n");
+    writer.write(valorMaximo + "\n");
+    
+    for (int y = 0; y < altura; y++) {
+        for (int x = 0; x < largura; x++) {
+            int bits = scanner.nextInt();
+            if (bits == 0) {
+            	newBit = 1;
+            }else {
+            	newBit = 0;
+            }
+            writer.write(newBit + " ");
+        }
+        writer.write("\n");
+    }
+    writer.close();
+	
+	String teste = "OK";
+    
+    return teste;
+}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		String inputFile = "/home/adriano/Imagens/PDI/Entrada_EscalaCinza.pgm"; // Caminho da imagem de entrada
+		String inputFile2 = "/home/adriano/Imagens/PDI/Manipula1.pbm"; // Caminho da imagem de entrada
+		
+		Alterar_PGM_PBM(inputFile, 1, "P1", 128);
+		Negativo(inputFile2, "P1");
+		
+	}
+
+}
